6a41b4bd889940d6f666cb5df7a5e3b007b9c409
Jana Bernkopf <>
2021-04-25 19:15:20 +0200

    domácí úkol; while cyklus

--- main.js
+++ main.js
@@ -6,10 +6,11 @@
 console.log('První příklad - vzestupná řada 0 - 10');
 
 //TADY NAPIS CYKLUS
-for (i = 0; i <= 10; i++) {
-    console.log(i);
+let a = 0;
+while (a < 10) {
+    a++;
+    console.log(a);
 }
-
 
 
 // Druhý příklad - zkopíruj předchozí příklad
@@ -19,16 +20,19 @@ for (i = 0; i <= 10; i++) {
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
-for (i = 0; i < 10; i++) {
-    console.log(i);
+let b = 0;
+while (b <= 10) {
+    b++;
+    console.log(b);
 }
-
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
-for (i = 10; i >= 0; i--) {
-    console.log(i);
+let c = 11;
+while (c > 0) {
+    c--;
+    console.log(c);
 }
